24f2e96aca879fd509cf21e477d38098ba4aba80
Syed Muneeb Ur Rehman Hashmi <syedmuneeburrehman@outlook.com>
2021-01-13 18:53:29 +0500

    functionality added

--- todoapp/api/views.py
+++ todoapp/api/views.py
@@ -19,7 +19,7 @@ def api_overview(request):
 
 @api_view(['GET'])
 def task_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-updated_at')
     serializer = TaskSerializer(tasks, many = True)
     return Response(serializer.data)
 
